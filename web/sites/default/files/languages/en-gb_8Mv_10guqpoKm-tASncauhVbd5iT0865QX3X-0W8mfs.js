cf0ec23d683d5d69edc57aee89c6ce1a50acd101
Ulka <ulka@Ulkas-MacBook-Pro.local>
2024-02-16 08:11:29 +0530

    first commit

---
+++ web/sites/default/files/languages/en-gb_8Mv_10guqpoKm-tASncauhVbd5iT0865QX3X-0W8mfs.js
@@ -0,0 +1,1 @@
+window.drupalTranslations = {"strings":{"":{"Status message":"Status message","Error message":"Error message","Warning message":"Warning message","Cancel":"Cancel","Open":"Open","Continue":"Continue"}},"pluralFormula":{"1":0,"default":1}};
